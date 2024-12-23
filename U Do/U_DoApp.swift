60300ded69c8040278a07825d3f63338792e9828
yoyojunn <96855738+yoyojunn@users.noreply.github.com>
2024-12-23 23:33:14 +0800

    Initial Commit

---
+++ U Do/U_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  U_DoApp.swift
+//  U Do
+//
+//  Created by yoyojun on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct U_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

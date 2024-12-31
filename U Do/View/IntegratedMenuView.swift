ef31eb6316730f10dd9d927467d3d093a51d4dc1
yoyojunn <96855738+yoyojunn@users.noreply.github.com>
2024-12-31 13:04:03 +0800

    Integrate settings in same window.

---
+++ U Do/View/IntegratedMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  IntegratedMenuView.swift
+//  U Do
+//
+//  Created by yoyojun on 31/12/2024.
+//
+
+import SwiftUI
+
+struct IntegratedMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntegratedMenuView()
+}
